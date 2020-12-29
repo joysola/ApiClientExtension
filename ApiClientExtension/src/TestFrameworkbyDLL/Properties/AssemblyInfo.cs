151aa3f97e1b10792daae50116a0a13f98a30706
joysola <chenwenjun@deepsight.top>
2020-12-29 16:34:55 +0800

    1. 引用HttpClientEX程序集，需要英勇aop、csharp、newtonjson这三个包。

--- ApiClientExtension/src/TestFrameworkbyDLL/Properties/AssemblyInfo.cs
+++ ApiClientExtension/src/TestFrameworkbyDLL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ApiClientExtension")]
+[assembly: AssemblyTitle("TestFrameworkbyDLL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApiClientExtension")]
+[assembly: AssemblyProduct("TestFrameworkbyDLL")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("847d76a8-42c4-42f1-aa36-3a7b90b0b82c")]
+[assembly: Guid("cd38ff41-6784-4905-882e-e78e6ea51b5b")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
---
+++ ApiClientExtension/src/TestFrameworkbyDLL/TestApi/LoginApi.cs
@@ -0,0 +1,28 @@
+﻿
+using HttpClientExtension.ApiClient;
+using HttpClientExtension.Attribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using TestModel;
+
+namespace TestFrameworkbyDLL
+{
+    /// <summary>
+    /// 登录所需Api
+    /// </summary>
+    public class LoginApi : BaseApi<LoginApi>
+    {
+        /// <summary>
+        /// 登录获取token
+        /// </summary>
+        /// <param name="postLoginModel"></param>
+        /// <returns></returns>
+        [Url("api/deepsight-auth/oauth/login")]
+        [HttpPost]
+        public ApiResponse<LoginModel> Login([PostContent] QueryLoginModel postLoginModel) => GetResult();
+    }
+}
